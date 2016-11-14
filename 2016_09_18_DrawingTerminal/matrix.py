b319b4ca77622537d85d2025c32c0c7cc09070c0
Pierre Depaz <pierre.depaz@gmail.com>
2016-11-15 02:44:40 +0400

    Added Drawing in the Terminal code

---
+++ 2016_09_18_DrawingTerminal/matrix.py
@@ -0,0 +1,36 @@
+# builtin packages
+from sys import stdout
+from time import sleep
+import random
+import os
+
+#additional packages > install using pip install <NAMEOFPACKAGE>
+import noise
+from blessings import Terminal
+
+interval = 0.001
+
+term = Terminal()
+width = term.width
+height = term.height
+
+possible_locations_columns = []
+column_spacing = 7
+
+for x in range (0, int(width/column_spacing)):
+    possible_locations_columns.append(x*column_spacing)
+
+# get_rid_of_top_line = ""
+os.system("clear")
+
+def random_move_drawing():
+    while True:
+        with term.location(possible_locations_columns[random.randint(0, len(possible_locations_columns)-1)], random.randint(1, height-2)):
+            print(term.green+str(random.randint(0, 9)))
+            term.location()
+
+
+try:
+    random_move_drawing()
+except KeyboardInterrupt:
+    print(term.green+"bye!")
