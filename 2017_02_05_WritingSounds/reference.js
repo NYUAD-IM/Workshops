17550f8519def5518546c8444fca9a49af0167d7
Pierre Depaz <pierre.depaz@gmail.com>
2017-02-05 12:19:39 +0400

    added reference js html

--- 2017_02_05_WritingSounds/reference.js
+++ 2017_02_05_WritingSounds/reference.js
@@ -2,6 +2,7 @@ var mySample;
 
 function init(){
   mySample = new Tone.Player("./samples/albloom.mp4").toMaster();
+
   console.log('sample loaded!');
 }
 
@@ -13,6 +14,14 @@ function changePlaybackRate(rate){
   mySample.playbackRate = rate;
 }
 
+function changePlaybackStart(start){
+  mySample.loopStart = start;
+}
+
+function changePlaybackEnd(start){
+  mySample.loopEnd = start;
+}
+
 function toggleAutoplay(){
   mySample.loop = !mySample.loop;
 }
@@ -20,17 +29,15 @@ function toggleAutoplay(){
 
 
 var isReverbConnected = false;
-
 var myReverb = new Tone.Freeverb().toMaster();
 
 function toggleReverb(){
+  mySample.disconnect();
   if(!isReverbConnected){
     mySample.connect(myReverb);
     isReverbConnected = true;
   }else{
-    mySample.disconnect();
     mySample.toMaster();
-
     isReverbConnected = false;
   }
 }
@@ -40,37 +47,47 @@ var isDelayConnected = false;
 var myDelay = new Tone.PingPongDelay().toMaster();
 
 function toggleDelay(){
+  note.disconnect();
   if(!isDelayConnected){
     mySample.connect(myDelay);
     isDelayConnected = true;
   }else{
-    mySample.disconnect();
     mySample.toMaster();
-
     isDelayConnected = false;
   }
 }
 
+
+var chord = ["F3", "Ab3", "C4", "Db4", "Eb4", "F4", "Ab4"];
+
 var params = {
-    oscillator:{type:"triangle"},
+    oscillator:{type:"sine"},
     envelope:{
-      attack: 0.0125,
-      decay:0.025,
-      sustain:0.5,
-      release:1
+      attack: 0.5,
+      decay:0.5,
+      sustain:3,
+      release:15
+    },
+    modulation:{
+      type: "sine"
+    },
+    modulationEnvelope:{
+      attack: 2,
+      decay: 0.5,
+      sustain: 3,
+      release: 15
     }
   };
 
-var note = new Tone.Synth(params).toMaster();
+var note = new Tone.FMSynth(params).toMaster();
 
 function playNote(){
-
-  note.triggerAttackRelease("C4", 0.25);
+  note.triggerAttackRelease(chord[Math.floor(Math.random()*chord.length)], 0.25);
 }
 
 function playNoteRandom(){
   if(Math.random() > 0.25)
-    note.triggerAttackRelease(500+Math.random()*1000, 0.25);
+    note.triggerAttackRelease(chord[Math.floor(Math.random()*chord.length)], 0.25);
 }
 
 function playOtherNoteRandom(){
@@ -78,27 +95,36 @@ function playOtherNoteRandom(){
     note.triggerAttackRelease(100+Math.random()*500, 0.25);
 }
 
+Tone.Transport.bpm.value = 60;
+
 function repeatNote(){
   Tone.Transport.scheduleRepeat(function(time){
-    playOtherNoteRandom();
-  }, "0:2");
-  Tone.Transport.scheduleRepeat(function(time){
-  	playNoteRandom();
-  }, "0:2");
+    playNoteRandom();
+  }, "1m");
 
   Tone.Transport.loop = true;
   Tone.Transport.start();
 }
 
-// TODO: figure out how we only play downbeat
 function repeatOtherNote(){
   Tone.Transport.scheduleRepeat(function(time){
-    playOtherNoteRandom();
-  }, "0:2:2.5");
+    note.triggerAttackRelease(chord[Math.floor(Math.random()*chord.length)], '8n', '+4n');
+  }, "1m");
 }
 
 function stopTransport(){
   Tone.Transport.stop();
+}
+
+var myFilter = new Tone.Filter(200, "lowpass", -48).toMaster();
+
+function toggleFilter(){
+  note.disconnect();
+  note.connect(myFilter);
+}
+
+function changeFilterBand(value){
+  myFilter.frequency.value = value;
 }
 
 document.addEventListener("keypress", function(){
